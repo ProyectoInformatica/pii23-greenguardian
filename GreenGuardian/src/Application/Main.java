6929ac1a40041646b360141f825af6794c2d1d2f
Maylen Paredes <maylen020704@gmail.com>
2023-11-10 15:00:04 +0100

    bordes de login y registro

--- GreenGuardian/src/Application/Main.java
+++ GreenGuardian/src/Application/Main.java
@@ -3,32 +3,27 @@ package Application;
 import java.io.IOException;
 
 import Application.controller.Controller;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
 public class Main extends Application{
 
 	@Override
+	@Override
 	public void start(Stage primaryStage) {
-		
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/Application/view/Inicio.fxml"));
-			Controller control = new Controller();
-			loader.setController(control);
-			Parent root = loader.load();
-			primaryStage.setScene(new Scene(root));
-			primaryStage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
+	    try {
+	        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Application/view/Inicio.fxml"));
+	        Controller control = new Controller();
+	        loader.setController(control);
+	        Parent root = loader.load();
+
+	        Scene scene = new Scene(root);
+
+	        // Aplica un estilo CSS a la escena
+	        scene.getStylesheets().add(getClass().getResource("/Application/styles.css").toExternalForm());
+
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
 	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
 }
