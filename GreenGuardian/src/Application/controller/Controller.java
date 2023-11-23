3423a8f11ee49812294ca39c39437d9c57afd832
Joseph Vasquez Flores <josephvz17@gmail.com>
2023-11-23 18:05:15 +0100

    Funcionalidad del boton volver

--- GreenGuardian/src/Application/controller/Controller.java
+++ GreenGuardian/src/Application/controller/Controller.java
@@ -30,10 +30,14 @@ public class Controller {
     private Button btnRegistrar;
     
     @FXML
+    private Button btnVolverR;
+    
+    @FXML
     void abrirPantalla(MouseEvent event) {
     	
     }
 
+    Stage stage1 = new Stage();
     @FXML
     void abrirRegistro(ActionEvent event) {
     	try {
@@ -41,7 +45,7 @@ public class Controller {
         	Controller control = new Controller();
         	loader1.setController(control);
 			Parent root1 = loader1.load();
-			Stage stage1 = new Stage();
+			
 			stage1.setScene(new Scene(root1));
 			stage1.initModality(Modality.WINDOW_MODAL);
 			stage1.initOwner(((Node) (event.getSource())).getScene().getWindow());
@@ -51,4 +55,11 @@ public class Controller {
 			e.printStackTrace();
 		}
     }
+    
+    @FXML
+    void cerrarRegsitro(ActionEvent event) {
+    	Node source = (Node) event.getSource();
+    	Stage stage = (Stage) source.getScene().getWindow();    
+    	stage.close();                           
+    	}
 }
